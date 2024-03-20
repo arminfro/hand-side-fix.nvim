d81cd72fcfef086871c1f36b6bcaa43d7fbb0272
Armin Fröhlich <mail@arminfroehlich.de>
2024-03-20 21:23:04 +0100

    filter mixups which are keywords

--- lua/syntax-hand-fix/mixups.lua
+++ lua/syntax-hand-fix/mixups.lua
@@ -3,8 +3,9 @@ local M = {}
 
 local keyboards = require("syntax-hand-fix.keyboards")
 local keywords_mod = require("syntax-hand-fix.keywords")
+local utils = require("syntax-hand-fix.utils")
 
-local function create_mixups(layout, keyword)
+local function keyword_mixup(layout, keyword)
   local mixups = {}
   local keyword_len = #keyword
   for i = 1, keyword_len - 1 do
@@ -18,14 +19,18 @@ local function create_mixups(layout, keyword)
   return mixups
 end
 
-local function create_all_mixups(layout, keywords)
+local function keywords_mixups(layout, keywords)
   local all_mixups = {}
   for _, keyword in ipairs(keywords) do
     local keyword_mixups = {}
-    local mixups = create_mixups(layout, keyword)
+
+    local mixups = keyword_mixup(layout, keyword)
     for _, mixup in ipairs(mixups) do
-      table.insert(keyword_mixups, mixup)
+      if not utils.has_value(keywords, mixup) then
+        table.insert(keyword_mixups, mixup)
+      end
     end
+
     if #keyword_mixups > 0 then
       all_mixups[keyword] = keyword_mixups
     end
@@ -34,7 +39,7 @@ local function create_all_mixups(layout, keywords)
 end
 
 M.get = function(layout, language)
-  return create_all_mixups(layout, keywords_mod.languages[language])
+  return keywords_mixups(layout, keywords_mod.languages[language])
 end
 
 return M
---
+++ lua/syntax-hand-fix/utils.lua
@@ -0,0 +1,14 @@
+---@class KeywordsModule
+local M = {}
+
+M.has_value = function(tab, val)
+  for _, value in ipairs(tab) do
+    if value == val then
+      return true
+    end
+  end
+
+  return false
+end
+
+return M
