44ea311e5196d5466beaebc302e83dfd4c05f6fa
Armin Fröhlich <mail@arminfroehlich.de>
2024-03-23 12:33:04 +0100

    rename test file, impr desc

--- tests/syntax-hand-fix/setup_spec.lua
+++ tests/syntax-hand-fix/setup_spec.lua
@@ -6,17 +6,17 @@ describe("setup", function()
     assert(plugin.init("lua") == nil)
   end)
 
-  it("does not work with unsuppoted layout opts", function()
-    local setup_function = function()
+  it("does not work with default opts", function()
+    local myfunction = function()
       return plugin.setup({ layout = "uus" })
     end
-
     local ends_with = function(str, suffix)
       return str:sub(-#suffix) == suffix
     end
 
-    xpcall(setup_function, function(err)
+    xpcall(myfunction, function(err)
       assert(ends_with(err, " not supported"))
+      -- assert() == some_how_expressing_it_errors)
     end)
   end)
 end)
