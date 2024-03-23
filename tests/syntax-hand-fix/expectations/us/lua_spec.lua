4776e20f2ab0d4b6d91d98dc1962bbcb5bb8fd5e
Armin Fröhlich <mail@arminfroehlich.de>
2024-03-23 12:41:37 +0100

    split test data definition into seperate files

---
+++ tests/syntax-hand-fix/expectations/us/lua_spec.lua
@@ -0,0 +1,22 @@
+--- here the table for us lua should be returned (from expected_mixups.lua file)
+return {
+  ["and"] = { "nad", "adn" },
+  ["break"] = { "breka" },
+  ["do"] = { "od" },
+  ["else"] = { "lese", "esle" },
+  ["elseif"] = { "leseif", "esleif", "elsief", "elsefi" },
+  ["end"] = { "ned", "edn" },
+  ["false"] = { "flase", "fasle" },
+  ["for"] = { "ofr", "fro" },
+  ["function"] = { "ufnction", "fucntion", "funciton" },
+  ["if"] = { "fi" },
+  ["local"] = { "lcoal", "locla" },
+  ["not"] = { "nto" },
+  ["or"] = { "ro" },
+  ["repeat"] = { "rpeeat", "reepat" },
+  ["return"] = { "reutrn", "retrun", "retunr" },
+  ["then"] = { "hten", "tehn", "thne" },
+  ["true"] = { "ture", "treu" },
+  ["until"] = { "utnil", "unitl" },
+  ["while"] = { "hwile", "whiel" },
+}
